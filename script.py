99b321be8f13dfde99c0ebce9ecf080f3038b79d
Aswankannan <88327644+Aswankannan@users.noreply.github.com>
2021-08-23 06:50:36 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,69 +1,33 @@
 class script(object):
 
 
-    START_MSG = """ <b>Hi {}
+    START_MSG = """ <b>ഡാ {}
 
-You can call this as an Auto Filter Bot if you like :D
+എൻ്റെ പേര് സുന്ദരൻ ☺️
+നിനക്ക് എന്നേ നിൻ്റെ ഗ്രൂപ്പിൽ ചേർക്കാൻ പറ്റില്ല അതുകൊണ്ട് സ്തലം വിട്ടോ നീ
 
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+<b>TATA BYE</b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
 
 <i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
+എനിക് നിന്നെ സഹായിക്കാൻ പറ്റില്ല 😈
 </i>
 
 
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+<b>Bot Commands - ഒന്ന് ഇറങ്ങി പോടാ</b>
 
 
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
+<b>© @Cybermediaworks/b>"""
 
 
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : സുന്ദരൻ</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Cybermediaworks
 
-⭕️<b>Language :</b> <code>Python3</code>
+⭕️<b>Language :</b> അതെന്തിന നീ അറിയുന്നെ
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>Tutorial Video :</b>ഒന്ന് പോടാ പുല്ലേ ഇറങ്ങി
 
 """
